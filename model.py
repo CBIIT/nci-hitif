6660dc79070cd2565d913561c56c0b4eca613193
George Fouad Ibrahim Zaki <zakigf@cn3164.(none)>
2017-09-18 11:37:20 -0400

    Added image augmentation option before fitting

--- model.py
+++ model.py
@@ -8,6 +8,7 @@ from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint,ReduceLROnPlateau,EarlyStopping, CSVLogger
 from keras import backend as K
 from keras.models import load_model
+from keras.preprocessing.image import ImageDataGenerator
 import argparse
 import pickle
 
@@ -82,7 +83,8 @@ def get_unet (n_layers=5, filter_size=32, dropout=None, activation_func="relu", 
 
     return model
 
-def train(model):
+
+def get_images(augmentation_factor=None):
 
     print('-'*30)
     print('Loading and preprocessing train data...')
@@ -94,36 +96,59 @@ def train(model):
     imgs_train_path=os.path.join(file_path, "data_python", imgs_train_file)
     imgs_mask_path=os.path.join(file_path, "data_python", imgs_mask_file)
 
-
     imgs_train = np.load(imgs_train_path)
     imgs_mask_train = np.load(imgs_mask_path)
 
-    #imgs_train = np.load('./data_python/1CDT_Green_Red_FarRed_Annotated_FISH_Dilation4Conn1Iter_Training_128by128_normalize.npy')
-    #imgs_mask_train = np.load('./data_python/1CDT_Green_Red_FarRed_Annotated_FISH_Dilation4Conn1Iter_Training_128by128_normalize_Mask.npy')
-
-
     imgs_train = imgs_train.astype('float32')
-    #mean = np.mean(imgs_train)  # mean for data centering
-    #std = np.std(imgs_train)  # std for data normalization
-
-    #imgs_train -= mean
-    #imgs_train /= std
-
     imgs_mask_train = imgs_mask_train.astype('float32')
     imgs_mask_train /= 255.  # scale masks to [0, 1]
 
-   
-#    model = get_unet()
-#
-#    if os.path.isfile(oldmodelwtsfname) and reloadFlag :
-#       print('-'*30)
-#       print('Loading previous weights ...')
-#       model.load_weights(oldmodelwtsfname)
+    print (np.shape(imgs_train))
+    print (np.shape(imgs_mask_train))
 
 
-# G.Z. already done when mode is passed 
-#    model.compile(optimizer=Adam(lr=1e-5), loss=dice_coef_loss, metrics=[dice_coef])
-#    model.summary()
+    imgs_train = np.expand_dims(imgs_train, axis= 3)
+    imgs_mask_train = np.expand_dims(imgs_mask_train, axis= 3)
+
+    return_images = imgs_train 
+    return_masks = imgs_mask_train 
+     
+    if augmentation_factor != None:
+    
+        sample_size = len(imgs_train) 
+        augmented_sample_size = int(float(sample_size) * augmentation_factor)
+        print ("Expanding the images from:{0} to {1}\n".format(sample_size, augmented_sample_size))
+    
+        data_gen_args = dict( rotation_range=90.,
+                width_shift_range=0.1,
+                height_shift_range=0.1,
+                )
+        image_datagen = ImageDataGenerator(**data_gen_args)
+        mask_datagen = ImageDataGenerator(**data_gen_args)
+    
+        augmented_images = []
+        augmented_masks = []
+    
+        i = 0     
+        for batch in image_datagen.flow(imgs_train, batch_size=1, seed=1337): 
+            augmented_images.append(np.squeeze(batch, axis=0))
+            i = i + 1 
+            if i >= augmented_sample_size:
+                break
+    
+        i = 0     
+        for batch in image_datagen.flow(imgs_mask_train, batch_size=1, seed=1337): 
+            augmented_masks.append(np.squeeze(batch,axis=0))
+            i = i +1
+            if i >=  augmented_sample_size:
+                break
+
+        return_images = np.stack(augmented_images, axis=0)
+        return_masks = np.stack(augmented_masks, axis=0)
+
+    return [return_images, return_masks]
+
+def train(model, imgs_train, imgs_mask_train):
 
     model_checkpoint = ModelCheckpoint(modelwtsfname, monitor='val_loss', save_best_only=True)
     reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1,patience=50, min_lr=0.001,verbose=1)
@@ -133,8 +158,11 @@ def train(model):
     print('-'*30)
     print('Fitting model...')
     print('-'*30)
-    imgs_train = np.expand_dims(imgs_train, 3)
-    imgs_mask_train = np.expand_dims(imgs_mask_train, 3)
+
+    print (np.shape(imgs_train))
+    print (np.shape(imgs_mask_train))
+    exit()
+    #return model.fit(imgs_train, imgs_mask_train, batch_size=2, epochs=3000, verbose=2, shuffle=True,
     return model.fit(imgs_train, imgs_mask_train, batch_size=2, epochs=3000, verbose=2, shuffle=True,
               validation_split=0.10, callbacks=[model_checkpoint, reduce_lr, model_es, csv_logger])
 
@@ -169,28 +197,35 @@ def predict(model):
     imgs_mask_test = model.predict(imgs_test, batch_size=256,verbose=1)
     np.save('/data/gudlap/koh/spot_learners_deeplearning/data_python/1CDT_Green_Red_Annotated_FISH_NoDillations__Testing_128by128_normalize_Mask_Pred_short.npy', np.squeeze(imgs_mask_test))
 
+
+
+
 if __name__ == '__main__':
 
 
     parser = argparse.ArgumentParser(description="spot learner")
-
-
-#def get_unet (n_layers=5, filter_size=32, dropout=True, activation_func="relu", conv_size=3 ):
 
     parser.add_argument('--nlayers', default=5, type = int, dest='n_layers', help="The number of layer in the forward path ")
     parser.add_argument('--num_filters', default=32, type = int, dest='num_filters', help="The number of convolution filters in the first layer")
     parser.add_argument('--conv_size', default='3', type = int, dest='conv_size', help="The convolution filter size.")
     parser.add_argument('--dropout', default=None, type = float, dest='dropout', help="Include a droupout layer with a specific dropout value.")
     parser.add_argument('--activation', default='relu',dest='activation', help="Activation function.")
+    parser.add_argument('--augmentation', default=None, type = float, dest='augmentation', help="Augmentation factor for the training set.")
 
+            
     args = parser.parse_args()
+
+    images , masks = get_images(args.augmentation)
 
     model = get_unet(n_layers=args.n_layers, \
                      dropout=args.dropout, \
                      filter_size=args.num_filters, \
                      activation_func=args.activation, \
                      conv_size=args.conv_size)
-    history_callback = train(model)
+
+
+    
+    history_callback = train(model, images, masks)
     print("Best val_dice_coef:")
     print(max(history_callback.history["val_dice_coef"]))
     #Save the history as pickle object
