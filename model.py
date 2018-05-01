3e90a40d6f7c6481ef84260a909b58672ce03647
George Fouad Ibrahim Zaki <zakigf@biowulf.nih.gov>
2018-05-01 09:01:40 -0400

    Added option for deployment and basic image preprocessing

--- model.py
+++ model.py
@@ -38,13 +38,25 @@ def dice_coef_loss(y_true, y_pred):
 
 
 
-def get_unet (img_rows, img_cols, n_layers=5, filter_size=32, dropout=None, activation_func="relu", conv_size=3 ):
+#def get_unet (img_rows, img_cols, n_layers=5, filter_size=32, dropout=None, activation_func="relu", conv_size=3, loss='dice'):
+def get_unet (img_rows, img_cols, args):
 
     print('-'*30)
     print('Creating and compiling model...')
     print('-'*30)
     print (img_rows)
     print (img_cols)
+
+    print (args)
+    n_layers = args.nlayers
+    filter_size = args.num_filters
+    dropout = args.dropout
+    activation_func = args.activation
+    conv_size = args.conv_size
+    loss_func = args.loss_func
+    last_activation = args.last_act
+
+
     inputs = Input((img_rows, img_cols, 1))
     conv_layers=[] 
     pool_layers=[inputs]
@@ -74,16 +86,53 @@ def get_unet (img_rows, img_cols, n_layers=5, filter_size=32, dropout=None, acti
         filter_size /= 2
 
 
-    last_conv =  Conv2D(1, (1, 1), activation='sigmoid')(conv_layers[-1])
+    #For binary classification, last activation should be sigmoid. 
+#    if loss_func  == 'dice':
+#        last_activation = 'sigmoid'
+#    else:
+#        print ("WARNING: last_activation set to None")
+#        last_activation = None
+
+    last_conv =  Conv2D(1, (1, 1), activation=last_activation)(conv_layers[-1])
     conv_layers.append(last_conv)
 
     model = Model(inputs=[inputs], outputs=[last_conv])
 
-
-    model.compile(optimizer=Adam(lr=1e-5), loss=dice_coef_loss, metrics=[dice_coef])
+    if loss_func == 'dice':
+        model.compile(optimizer=Adam(lr=1e-5), loss=dice_coef_loss, metrics=[dice_coef])
+    else:
+        #Any Keras loss function will be passed
+        model.compile(optimizer=Adam(lr=1e-5), loss = loss_func)
     model.summary()
-
     return model
+
+def preprocess_images(images):
+    imgs_train = np.squeeze(np.load(images))
+    if imgs_train.ndim != 3:
+        raise Exception("Error: The number of dimensions for images should equal 3, after squeezing the shape is:{0}".format(np.shape(images)))
+    imgs_train = imgs_train.astype('float32')
+    print("MAX before:{0}".format(np.amax(imgs_train)))
+    #Normalize all number between 0 and 1.
+    uint16_info = np.iinfo('uint16')
+    imgs_train = imgs_train / uint16_info.max
+    print("MAX after:{0}".format(np.amax(imgs_train)))
+    imgs_train = np.expand_dims(imgs_train, axis= 3)
+
+    return imgs_train
+
+
+def preprocess_masks(masks, normalize_mask=False):
+    
+    imgs_mask_train = np.squeeze(np.load(masks))
+    if imgs_mask_train.ndim != 3:
+        raise Exception("Error: The number of dimensions for masks should equal 3, after squeezing the shape is:{0}".format(np.shape(masks)))
+
+    imgs_mask_train = imgs_mask_train.astype('float32')
+    if normalize_mask:
+        imgs_mask_train /= 255.  # scale masks to [0, 1]
+
+    imgs_mask_train = np.expand_dims(imgs_mask_train, axis= 3)
+    return imgs_mask_train
 
 
 def get_images(images, masks, augmentation_factor=None, normalize_mask=False):
@@ -92,35 +141,39 @@ def get_images(images, masks, augmentation_factor=None, normalize_mask=False):
     print('Loading and preprocessing train data...')
     print('-'*30)
 
-    #imgs_train_file="1CDT_Green_Red_FarRed_Annotated_FISH_Dilation4Conn1Iter_Training_128by128_normalize.npy"
-    #imgs_mask_file="1CDT_Green_Red_FarRed_Annotated_FISH_Dilation4Conn1Iter_Training_128by128_normalize_Mask.npy"
-    #file_path = os.path.dirname(os.path.realpath(__file__))
-    #imgs_train_path=os.path.join(file_path, "data_python", imgs_train_file)
-    #imgs_mask_path=os.path.join(file_path, "data_python", imgs_mask_file)
+#    imgs_train = np.squeeze(np.load(images))
+#    imgs_mask_train = np.squeeze(np.load(masks))
+#
+#
+#    if imgs_train.ndim != 3:
+#        raise Exception("Error: The number of dimensions for images should equal 3, after squeezing the shape is:{0}".format(np.shape(images)))
+#
+#    if imgs_mask_train.ndim != 3:
+#        raise Exception("Error: The number of dimensions for masks should equal 3, after squeezing the shape is:{0}".format(np.shape(masks)))
+#
+#    imgs_train = imgs_train.astype('float32')
+#    imgs_mask_train = imgs_mask_train.astype('float32')
+#    if normalize_mask:
+#        imgs_mask_train /= 255.  # scale masks to [0, 1]
+#
+#
+#    imgs_train = np.expand_dims(imgs_train, axis= 3)
+#    imgs_mask_train = np.expand_dims(imgs_mask_train, axis= 3)
 
+    imgs_train = preprocess_images(images) 
+    imgs_mask_train = preprocess_masks(masks, normalize_mask) 
 
-    imgs_train = np.squeeze(np.load(images))
-    imgs_mask_train = np.squeeze(np.load(masks))
+    #Shuffle the images
+    np.random.seed(10)
+    shuffled_id = np.random.permutation(imgs_train.shape[0])
+    imgs_train = imgs_train[shuffled_id]
+    imgs_mask_train = imgs_mask_train[shuffled_id]
 
-
-    if imgs_train.ndim != 3:
-        raise Exception("Error: The number of dimensions for images should equal 3, after squeezing the shape is:{0}".format(np.shape(images)))
-
-    if imgs_mask_train.ndim != 3:
-        raise Exception("Error: The number of dimensions for masks should equal 3, after squeezing the shape is:{0}".format(np.shape(masks)))
-
-    imgs_train = imgs_train.astype('float32')
-    imgs_mask_train = imgs_mask_train.astype('float32')
-    if normalize_mask:
-        imgs_mask_train /= 255.  # scale masks to [0, 1]
-
-
-    imgs_train = np.expand_dims(imgs_train, axis= 3)
-    imgs_mask_train = np.expand_dims(imgs_mask_train, axis= 3)
-
+    assert(np.amax(imgs_mask_train) == 1)
+    assert(np.amin(imgs_mask_train) == 0)
     return_images = imgs_train 
     return_masks = imgs_mask_train 
-     
+
     if augmentation_factor != None:
     
         sample_size = len(imgs_train) 
@@ -164,7 +217,7 @@ def train(model, imgs_train, imgs_mask_train, initialize=None):
 
     model_checkpoint = ModelCheckpoint(modelwtsfname, monitor='val_loss', save_best_only=True)
     reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1,patience=50, min_lr=0.001,verbose=1)
-    model_es = EarlyStopping(monitor='val_loss', min_delta=0.00001, patience=50, verbose=1, mode='auto')
+    model_es = EarlyStopping(monitor='val_loss', min_delta=0.000001, patience=50, verbose=1, mode='auto')
     csv_logger = CSVLogger('training.csv')
 
     print('-'*30)
@@ -181,14 +234,14 @@ def train(model, imgs_train, imgs_mask_train, initialize=None):
     return model.fit(imgs_train, imgs_mask_train, batch_size=2, epochs=3000, verbose=2, shuffle=True,
               validation_split=0.10, callbacks=[model_checkpoint, reduce_lr, model_es, csv_logger])
 
-def predict(model):
+def predict(model, weights, images):
     print('-'*30)
     print('Loading and preprocessing test data...')
     print('-'*30)
 
-    imgs_test = np.load('./data_python/1CDT_Green_Red_FarRed_Annotated_FISH_Dilation4Conn1Iter_Testing_128by128_normalize.npy')
-    imgs_mask_test = np.load('.//data_python/1CDT_Green_Red_FarRed_Annotated_FISH_Dilation4Conn1Iter_Testing_128by128_normalize_Mask.npy')
-    imgs_test = imgs_test.astype('float32')
+    #imgs_test = np.load('./data_python/1CDT_Green_Red_FarRed_Annotated_FISH_Dilation4Conn1Iter_Testing_128by128_normalize.npy')
+    #imgs_mask_test = np.load('.//data_python/1CDT_Green_Red_FarRed_Annotated_FISH_Dilation4Conn1Iter_Testing_128by128_normalize_Mask.npy')
+    #imgs_test = imgs_test.astype('float32')
 
     #imgs_train = np.load('../data_python/1CDT_Green_Red_Annotated_FISH_Dilation8Conn1Iter_Training_128by128.npy')
     #imgs_train = imgs_train.astype('float32')
@@ -198,20 +251,21 @@ def predict(model):
     #imgs_test -= mean
     #imgs_test /= std
 
+
+
     print('-'*30)
     print('Loading saved weights...')
     print('-'*30)
 
-    model = get_unet_short()
-    model.load_weights(modelwtsfname)
+    #model = get_unet_short()
+    model.load_weights(weights)
 
     print('-'*30)
     print('Predicting masks on test data...')
     print('-'*30)
-    imgs_test = np.expand_dims(imgs_test,3)
-    imgs_mask_test = model.predict(imgs_test, batch_size=256,verbose=1)
-    np.save('/data/gudlap/koh/spot_learners_deeplearning/data_python/1CDT_Green_Red_Annotated_FISH_NoDillations__Testing_128by128_normalize_Mask_Pred_short.npy', np.squeeze(imgs_mask_test))
-
+    #imgs_test = np.expand_dims(imgs_test,3)
+    imgs_mask_test = model.predict(images, batch_size = 64,verbose=1)
+    np.save('mask_predictions.npy', np.squeeze(imgs_mask_test))
 
 def evaluate_params(args): 
 
@@ -222,11 +276,7 @@ def evaluate_params(args):
     img_cols = np.shape(images)[2]
         
     #np.save("augmented-images.npy", np.squeeze(images))
-    model = get_unet(img_rows, img_cols, n_layers=args.n_layers, \
-                     dropout=args.dropout, \
-                     filter_size=args.num_filters, \
-                     activation_func=args.activation, \
-                     conv_size=args.conv_size)
+    model = get_unet(img_rows, img_cols, args)
 
     history_callback = train(model, images, masks, initialize=args.initialize)
     return history_callback
@@ -239,21 +289,39 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="spot learner")
 
     parser.add_argument('images',  help="The 2d numpy array image stack or 128 * 128")
-    parser.add_argument('masks',  help="The 2d numpy array mask (8bits) stack or 128 * 128")
-    parser.add_argument('--nlayers', default=5, type = int, dest='n_layers', help="The number of layer in the forward path ")
+    parser.add_argument('masks',  help="The 2d numpy array mask (16bits) stack or 128 * 128")
+    parser.add_argument('--nlayers', default=5, type = int, dest='nlayers', help="The number of layer in the forward path ")
     parser.add_argument('--num_filters', default=32, type = int, dest='num_filters', help="The number of convolution filters in the first layer")
     parser.add_argument('--conv_size', default='3', type = int, dest='conv_size', help="The convolution filter size.")
     parser.add_argument('--dropout', default=None, type = float, dest='dropout', help="Include a droupout layer with a specific dropout value.")
     parser.add_argument('--activation', default='relu',dest='activation', help="Activation function.")
     parser.add_argument('--augmentation', default=None, type = float, dest='augmentation', help="Augmentation factor for the training set.")
     parser.add_argument('--initialize', default=None, dest='initialize', help="Numpy array for weights initialization.")
-    parser.add_argument('--normalize_mask', action='store_true', dest='normalize_mask', help="Normalize the mask to 0-1 by dividing by 255.")
+    parser.add_argument('--normalize_mask', action='store_true', dest='normalize_mask', help="Normalize the mask in case of uint8 to 0-1 by dividing by 255.")
+    parser.add_argument('--predict', action='store_true', dest='predict', help="Use the model passed in initialize to perform segmentation")
+    parser.add_argument('--loss_func', default='dice', dest='loss_func', help="Keras supported loss function, or 'dice'. ")
+    parser.add_argument('--last_act', default='sigmoid', dest='last_act', help="The activation function for the last layer.")
+        
+
 
             
     args = parser.parse_args()
-    history_callback = evaluate_params(args)
-    print("Best val_dice_coef:")
-    print(max(history_callback.history["val_dice_coef"]))
-    #Save the history as pickle object
-    pickle.dump(history_callback.history, open( "fit_history.p", "wb" ) )
-#    predict()
+    if not args.predict:
+        history_callback = evaluate_params(args)
+        print("Minimum validation loss:")
+        print(min(history_callback.history["val_loss"]))
+        #Save the history as pickle object
+        pickle.dump(history_callback.history, open( "fit_history.p", "wb" ) )
+    else:
+        if args.initialize is None:
+            raise Exception("Error: The initialize option has to be set for prediction")
+
+        #It is not necessary to pass masks for prediction, but I am just following the function
+        #prototype for now.
+        images = preprocess_images(args.images)
+        #Get the images size  
+        img_rows = np.shape(images)[1]
+        img_cols = np.shape(images)[2]
+        model = get_unet(img_rows, img_cols, args)
+        weights = args.initialize
+        predict(model, weights, images)
