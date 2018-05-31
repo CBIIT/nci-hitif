37c8ed38eca81200fad0b4cf371bf25070bd8ceb
George Fouad Ibrahim Zaki <zakigf@biowulf.nih.gov>
2018-05-31 08:48:15 -0400

    added utilities functions

--- src/utils.py
+++ src/utils.py
@@ -8,15 +8,32 @@ class augmented_predict:
         self.array = np.copy(images)
         self.stacks["array"] = self.array
         self.nslides = np.shape(self.array)[0]
+        self.all_aug_input =  None
 
-    def get_augmented_patch(self):
+    def augment_inputs(self):
         self.stacks["rot90"] = np.rot90(self.array, k = 1, axes = (1,2))
         self.stacks["rot180"] = np.rot90(self.array, k = 2, axes = (1,2))
         self.stacks["rot270"] = np.rot90(self.array, k = 3, axes = (1,2))
         self.stacks["lr"] = self.array[:,...,::-1]   
         self.stacks["ud"] = self.array[:,::-1,...] 
-        all_aug_input =  self.__pack()
-        return all_aug_input
+        self.all_aug_input =  self.__pack()
+
+    def get_augmented_patch(self):
+        if self.all_aug_input is not  None:
+            return self.all_aug_input 
+        else:
+            raise Exception("Augmented patch not generated")
+        #for image in self.all_aug_input:
+        #    yield image 
+
+    def get_n_aug_stacks(self):
+        """
+        Returns the number of augmented images
+        """
+        if self.all_aug_input is not  None:
+            return np.shape(self.all_aug_input)[0]
+        else:
+            raise Exception("Augmented patch not generated")
 
     def __pack(self):
         self.packing_order = ["array", "rot90", "rot180", "rot270", "lr", "ud"]
@@ -53,6 +70,9 @@ class augmented_predict:
 if __name__ == "__main__":
     u = np.arange(8).reshape((2,2,2))
     f = augmented_predict(u)
+    f.augment_inputs()
+    n_stacks = f.get_n_aug_stacks()
+    #print n_stacks
     x = f.get_augmented_patch()
     print x
     print f.reduce_predictions(x)
