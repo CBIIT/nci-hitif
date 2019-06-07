7b5c017cb70c8aa30df93a3a9b1fed581a3c7954
George Zaki <george.zaki@nih.gov>
2019-06-07 08:07:51 -0400

    converted to python 3

--- test/test_grid/generate_test.py
+++ test/test_grid/generate_test.py
@@ -4,8 +4,8 @@ masks = np.zeros((10,10), dtype = np.uint8)
 
 masks[0:4, 2:5] = 128
 masks[1:5, 3:9] = 129
-print masks
-print masks.shape
+print(masks)
+print(masks.shape)
 
 #Save masks as image
 from skimage.io import imsave
