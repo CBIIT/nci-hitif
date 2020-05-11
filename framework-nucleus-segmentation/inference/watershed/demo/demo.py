bb84938343256af2690e78de8500b5182bfd4bf3
Kyunghun Lee <leekh3@umd.edu>
2020-05-11 12:17:55 -0400

    doc updated

---
+++ framework-nucleus-segmentation/inference/watershed/demo/demo.py
@@ -0,0 +1,40 @@
+import sys,glob
+import matplotlib.pyplot as plt
+from PIL import Image
+from skimage.transform import resize
+from skimage.util import img_as_ubyte
+from skimage.color import label2rgb
+
+sys.path.insert(1, '../src');
+sys.path.insert(1,'../../../visualization')
+from watershed_infer import *
+from download_util import *
+
+BLUR_MODEL_URL = 'https://ndownloader.figshare.com/files/22280349?private_link=a3fec498ef6d08ac6973'
+BLUR_MODEL_PATH = 'blurred_border_FPN_pretrained.zip'
+download_and_unzip_datasets(BLUR_MODEL_URL, BLUR_MODEL_PATH)
+
+modelwtsfname = "./blurred_border_FPN_pretrained.npy"
+modeljsonfname = "./blurred_border_FPN_pretrained.json"
+gaussian_blur_model = get_model(modeljsonfname,modelwtsfname)
+
+DISTANCE_MAP_MODEL_URL = 'https://ndownloader.figshare.com/files/22280352?private_link=5b1454e3f3bd23dea56f'
+DISTANCE_MAP_MODEL_PATH = 'distance_map_FPN_pretrained.zip'
+download_and_unzip_datasets(DISTANCE_MAP_MODEL_URL, DISTANCE_MAP_MODEL_PATH)
+
+modelwtsfname = "./distance_map_FPN_pretrained.npy"
+modeljsonfname = "./distance_map_FPN_pretrained.json"
+distance_map_model = get_model(modeljsonfname,modelwtsfname)
+
+config_file_path='./demo.ini'
+with open(config_file_path, 'r') as fin:
+    print(fin.read())
+
+image_list =['/Users/leek13/Downloads/1a.tif']
+
+img = np.zeros((len(image_list),1078,1278))
+for i in range(len(img)):
+    image_resized = img_as_ubyte(resize(np.array(Image.open(image_list[i])), (1078, 1278)))
+    img[i,:,:] = image_resized
+
+mask = watershed_infer(img,gaussian_blur_model,distance_map_model,config_file_path)
