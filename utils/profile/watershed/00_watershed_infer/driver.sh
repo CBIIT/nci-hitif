7f11c9819f179e4025626edeebeee964106dcc5f
Kyunghun Lee <leekyh3@gmail.com>
2020-05-12 09:55:00 -0400

    doc updated

--- utils/profile/watershed/00_watershed_infer/driver.sh
+++ utils/profile/watershed/00_watershed_infer/driver.sh
@@ -4,7 +4,7 @@ WATERSHED_SRC="../../../../framework-nucleus-segmentation/inference/watershed/sr
 cp $WATERSHED_DEMO/demo.py .
 cp $WATERSHED_SRC/watershed_infer.py .
 sed -i '/def watershed_infer(img,gaussian_blur_model,distance_map_model,config_file_path):/i @profile' watershed_infer.py
-sed -i 's/img = np.zeros((len(image_list),1078,1278))/img = np.zeros((100,1078,1278))/g' demo.py
+sed -i 's/img = np.zeros((len(image_list),1078,1278))/img = np.zeros((1,1078,1278))/g' demo.py
 sed -i 's/from watershed_infer import */from watershed_infer_profile import /g' demo.py
 sed -i '/import sys,glob,warnings,os/i import line_profiler' demo.py
 sed -i '/image_resized = img_as_ubyte/d' demo.py
@@ -19,5 +19,5 @@ RES_FOLDER=`pwd`
 pushd $WATERSHED_DEMO
 kernprof -l -v demo_profile.py > $RES_FOLDER/result.txt
 popd
-rm *.py
+#rm *.py
 
